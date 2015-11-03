5fc4f355b20fd5d3dceb332ced689006593edaf0
rodrigore <rodrigore@Rodrigos-Mac-mini.local>
2015-11-03 01:22:19 -0300

    Agregada rutas para los usuarios

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,11 +1,22 @@
 <?php
 
-get('/api/rodri', function () {
-    return response()->json(\App\User::find(1)->products);
-});
-
 get('rodri', function () {
     $products = \App\User::find(1)->products;
+    return view('list', compact('products'));
+});
+
+get('jp', function () {
+    $products = \App\User::find(2)->products;
+    return view('list', compact('products'));
+});
+
+get('aphix', function () {
+    $products = \App\User::find(4)->products;
+    return view('list', compact('products'));
+});
+
+get('amaury', function () {
+    $products = \App\User::find(3)->products;
     return view('list', compact('products'));
 });
 
